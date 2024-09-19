f2f792423421883b1dc491b1cc74d51d055307ab
shiva kharbanda <shiva.kharbanda@daaslabs.ai>
2024-09-19 19:02:38 +0530

    ship testing complted

--- ship.js
+++ ship.js
@@ -2,18 +2,21 @@ export class Ship{
     constructor(length, totalHits = 0, sunk = false) {
         this.length = length;
         this.totalHits = totalHits;
-        this.sunk = sunk;
+        this.sunk = false;
     }
 
     hit(){
-        this.totalHits += 1;
+        if (!this.sunk) {
+            this.totalHits += 1;
+        }
     }
 
     isSunk() {
-        if (this.hitTotal >= this.length) {
+        if (this.totalHits == this.length) {
             this.sunk = true;
+            return true
         }
-        return this.sunk;
+        return false
     }
 }
 
--- ship.test.js
+++ ship.test.js
@@ -5,3 +5,19 @@ test("shit init: happy path", ()=>{
 
     expect(ship.length).toBe(3);
 })
+
+test("ship hit test", ()=>{
+    let ship = new Ship(3);
+    ship.hit()
+    expect(ship.totalHits).toBe(1);
+})
+
+test("ship sink test", ()=>{
+    let ship = new Ship(3);
+    expect(ship.sunk).toBeFalsy();
+    ship.hit()
+    ship.hit()
+    expect(ship.sunk).toBeFalsy();
+    ship.hit()
+    expect(ship.isSunk()).toBeTruthy();
+})
