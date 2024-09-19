e6b568b061c12fe7babcd7547460918057652cbf
shiva kharbanda <shiva.kharbanda@daaslabs.ai>
2024-09-19 19:21:42 +0530

    ship upgrade

---
+++ src/gameBoard.js
@@ -0,0 +1,7 @@
+export class gameBoard {
+
+    constructor(size = 10, ships ) {
+
+    }
+
+}
--- src/ship.js
+++ src/ship.js
@@ -1,8 +1,29 @@
 export class Ship{
-    constructor(length, totalHits = 0, sunk = false) {
-        this.length = length;
+    constructor(name, totalHits = 0, sunk = false) {
+        this.shipMap = this.shipMapInit();
+
+        if (! this.shipMap.has(name)) {
+            throw new Error(`Invalid ship name: Valid ship names are : ${this.shipMap.keys}`);
+        }
+        this.name = name;
+        this.length = getLength();
         this.totalHits = totalHits;
         this.sunk = false;
+
+    }
+
+    getLength(){
+        return this.shipMap.get(this.name);
+    }
+
+    shipMapInit(){
+        let shipMap = new Map();
+        shipMap.set("carrier", 5);
+        shipMap.set("battleship", 4);
+        shipMap.set("cruiser", 3);
+        shipMap.set("submarine", 3)
+        shipMap.set("destroyer", 2)
+        return shipMap
     }
 
     hit(){
