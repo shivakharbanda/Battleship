ff37ce1bb98fbd5f61cf5d4e1c6f4c8ebb265b5d
shiva kharbanda <shiva.kharbanda@daaslabs.ai>
2024-09-21 20:33:37 +0530

    horizontal ship placing tests

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,7 +1,59 @@
+import { Ship } from "./ship";
+
 export class gameBoard {
 
-    constructor(size = 10, ships ) {
+    constructor(size = 10) {
+        this.board = [];
+        for (let i = 0;i < size ;i++ ) {
+            let row = new Array(size).fill(0);
+            this.board.push(row);
+        }
+    }
+    
+    placeShip(name, startCoordinate, orientation = "h") {
 
+        orientation = orientation.toLowerCase();
+
+        if (orientation != 'h' && orientation != 'v') {
+            throw new Error(`Value Error: Invalid Orientation value - ${orientation}`);
+            
+        }
+        let ship = new Ship(name);
+        if (orientation == "h") {
+            let row = startCoordinate[0];
+            this.checkIfShipAlreadyThere(startCoordinate, orientation, ship.length);
+
+            row = this.board[row]
+            let col = startCoordinate[1]
+             
+            for (let i = 0; i < ship.length; i ++) {
+                row[col] = ship.name;
+                col += 1;
+            }
+        }
+    }
+
+    checkIfShipAlreadyThere(startCoordinate, orientation, shipLength) {
+        if (orientation == "h") {
+            let row = startCoordinate[0];
+            let rowToCheck = this.board[row];
+
+            let columnStart = startCoordinate[1];
+
+            while(shipLength > 0) {
+                if (rowToCheck[columnStart] == undefined) {
+                    throw new Error(`not enough space to put the ship of length ${shipLength}`);
+                }
+                if (rowToCheck[columnStart] != 0) {
+                    throw new Error("Ship already at designated coordinate. Place it somewhere else");
+                }
+                
+                columnStart += 1;
+                shipLength -= 1;
+            }
+        }
+        
     }
 
 }
+
---
+++ src/tests/gameBoard.test.js
@@ -0,0 +1,65 @@
+import { gameBoard } from "../gameBoard"
+
+test("invalid ship name", ()=>{
+    let gameBoardObj = new gameBoard();
+
+    expect(gameBoardObj.board.length).toEqual(10);
+
+    expect(gameBoardObj.board[0].length).toEqual(10);
+})
+
+
+// case 1 happy path
+//      use a valid ship name and put that ship on a valid coordinate and orientation
+
+// case 2 big ship no size
+//      put the ship bigger than the size available
+
+// case 3 already placed ship
+//      put the ship but there is already a ship present where it is being set
+describe('Test the ship placing abilities in horizontal sense', () => {
+    test("place ship", ()=>{
+        let gameBoardObj = new gameBoard();
+        let coordinates = [0, 0]
+        gameBoardObj.placeShip("carrier", coordinates, "H")
+    
+        expect(gameBoardObj.board[coordinates[0]].slice(coordinates[1], coordinates[1] + 5)).toEqual(
+            ["carrier", "carrier", "carrier", "carrier", "carrier"]
+        )
+    })
+    test("get error if space not enough", ()=>{
+        
+        expect(()=>{
+                let gameBoardObj = new gameBoard();
+                let coordinates = [0, 6]
+                gameBoardObj.placeShip("carrier", coordinates, "H")
+            }
+        ).toThrowError();
+    })
+    test("ship over ship", ()=>{
+        
+        expect(()=>{
+                let gameBoardObj = new gameBoard();
+                let coordinates = [0, 0]
+                gameBoardObj.placeShip("carrier", coordinates, "H")
+                gameBoardObj.placeShip("battleship", [0, 2], "H")
+            }
+        ).toThrowError();
+    })
+
+    test("2 ships in one row", ()=>{
+        let gameBoardObj = new gameBoard();
+        let coordinatesShip1 = [0, 0];
+        let coordinateShip2 = [0, 5]
+
+        gameBoardObj.placeShip("carrier", coordinatesShip1, "H")
+        gameBoardObj.placeShip("battleship", coordinateShip2, "H")
+        expect(
+            gameBoardObj.board[coordinatesShip1[0]]
+        ).toEqual(
+            ["carrier", "carrier", "carrier", "carrier", "carrier", "battleship", "battleship", "battleship", "battleship", 0]
+        );
+    })
+    
+    
+})
